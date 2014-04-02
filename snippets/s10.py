450e76e3e863bdcdb5d5803a8806c2ac3013d482
preinh <preinh@gmail.com>
2014-04-02 19:17:03 -0300

    cknn: coupled k-nearest neighbor snippet

---
+++ snippets/s10.py
@@ -0,0 +1,77 @@
+import numpy as np
+from scipy import optimize
+
+from sklearn import neighbors as nn
+
+from matplotlib import pylab as pl
+
+k = 5
+a = 80
+
+_x = np.linspace(0, 100, 50)
+_y = np.linspace(0, 100, 50)
+
+xx, yy = np.meshgrid(_x, _y)
+
+X = np.array(zip(xx.ravel(), yy.ravel()))
+
+def knn(X, k, pos):
+    knn = nn.NearestNeighbors(n_neighbors=k, 
+                              metric='euclidean').fit(X)
+    #print knn.kneighbors(X, k, return_distance=True)
+    _d, _i = knn.kneighbors([0,0], k, return_distance=True)
+
+
+#    print _d, _i, X[_i]
+
+#             import scipy.spatial
+#             t = scipy.spatial.KDTree(X[1:])
+#             _d, _i = t.query([0,0], k)
+            #print np.ndarray(_d[0])
+#             print np.array(_i[0])
+#            print _d[0], _i[0], X[_i[0]]
+
+    
+#     pl.scatter(xx.ravel(), yy.ravel())
+#     pl.scatter(X[_i, 0], X[_i, 1], 100, 'y', 's', alpha=0.5)
+#     pl.xlim(xmin=0)
+#     pl.ylim(ymin=0)
+#     
+#     pl.show()
+
+    return max(  np.abs(X[_i, pos])  )
+
+#exit()
+
+
+def f(x, a, k):
+    return x[0] + a*x[1]
+
+def time_constraint(x, *kargs, **kwargs):
+    return x[0] - knn(X, k, pos=0)
+
+def space_constraint(x, *kargs, **kwargs):
+    return x[1] - knn(X, k, pos=1)
+
+
+x0 = np.array([1, 1])
+
+hi, di = optimize.fmin_slsqp(   f, 
+                                x0, 
+                                args=(a, k),
+                                ieqcons=[time_constraint, 
+                                         space_constraint],
+                                full_output=False)
+print hi, di
+
+    
+pl.scatter(xx.ravel(), yy.ravel(), c='y', marker='x')
+#pl.scatter(X[_i, 0], X[_i, 1], 100, 'y', 's', alpha=0.5)
+pl.xlim(xmin=0)
+pl.ylim(ymin=0)
+
+pl.axhline(di)
+pl.axvline(hi)
+
+pl.show()
+
